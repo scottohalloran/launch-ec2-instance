faad94aeed0862fb7ee31d26fa8224529d21dfcc
scottohalloran <o_halloran@yahoo.com>
2022-12-30 22:04:42 -0800

    Changes

--- cicd/apply-terraform.sh
+++ cicd/apply-terraform.sh
@@ -9,8 +9,8 @@ cd ..
 # initialize terraform
 terraform init
 
-# # apply terraform
-#terraform apply -auto-approve
+#apply terraform
+terraform apply -auto-approve
 
 # destroy terraform
- terraform destroy -auto-approve
+# terraform destroy -auto-approve
