19c8b181778462072fb43cb9b7328aea14c9cde8
scottohalloran <o_halloran@yahoo.com>
2022-12-31 20:36:25 -0800

    Test Terraform

--- provider.tf
+++ provider.tf
@@ -6,14 +6,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.22.0"
-    }
+      }
 
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
-    }
+      }
   }
 
-  required_version = "~> 1.0"
+  
 }
